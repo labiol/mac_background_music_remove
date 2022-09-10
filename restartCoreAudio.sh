121759e364444594644bd0b75c99e62c32c2d0a7
labiol <35596990+labiol@users.noreply.github.com>
2022-09-10 10:15:40 +0200

    Update restartCoreAudio.sh

--- restartCoreAudio.sh
+++ restartCoreAudio.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+#use before restarting your mac, usually helps in case of sound issue
+#it will disconnect all headset
+
 ps ax|grep 'coreaudio[a-z]' | awk '{print $1 " " $5}'
 kill -9 $(ps ax|grep 'coreaudio[a-z]' | awk '{print $1}')
